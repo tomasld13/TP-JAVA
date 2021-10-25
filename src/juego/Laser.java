3fc979fddbe3b91cd23b7df364a0fa4e65d9574f
tomasld13 <tomasld13@gmail.com>
2021-10-24 21:35:48 -0300

    Se crea el objeto laser y se unifica el trabajo

--- src/juego/Laser.java
+++ src/juego/Laser.java
@@ -1,5 +1,31 @@
 package juego;
 
-public class Laser {
+import java.awt.Image;
+import java.awt.Color;
+import entorno.Entorno;
+import entorno.Herramientas;
 
+public class Laser {
+	private int tamaño;
+	private double x;
+	private double y;
+	//private Image img;
+	
+	public Laser(int tamaño, double x, double y) {
+		this.tamaño = tamaño;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void dibujarLaser(Entorno e) {
+		e.dibujarTriangulo(x, y, tamaño, tamaño, Math.PI / 2, Color.yellow);
+	}
+	
+	public void lanzarLaser(boolean direccion) {
+		if(direccion) {
+			x+=5;
+		}else {
+			x-=5;
+		}
+	}
 }
